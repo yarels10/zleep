c590af09ea9182ef0d017d2279789f36453adb5b
Robert Pena <rapena4@miners.utep.edu>
2015-08-03 15:22:00 -0700

    made the reward store link to startup screen, also made reward item save in UserDefaults if bought

--- Zleep/Zleep/SetupViewController.m
+++ Zleep/Zleep/SetupViewController.m
@@ -9,7 +9,7 @@
 #import "SetupViewController.h"
 #import "ZleepManager.h"
 #import "inZleepViewController.h"
-
+#import "RewardsCollectionViewController.h"
 
 @interface SetupViewController ()
 @property (weak, nonatomic) IBOutlet UIDatePicker *startTime;
@@ -20,6 +20,19 @@
 @end
 
 @implementation SetupViewController
+
+
+
+
+
+- (IBAction)storeButtonHit:(id)sender {
+    RewardsCollectionViewController *destination = [[UIStoryboard storyboardWithName:@"Rewards" bundle:nil] instantiateInitialViewController];
+                                                
+               // [self.navigationController pusViewController:destination animated:YES];
+    
+    [self.navigationController pushViewController:destination animated:YES];
+    
+}
 - (IBAction)startButtonHit:(id)sender {
    
     NSDateFormatter *dateFormatter= [NSDateFormatter new];
@@ -51,9 +64,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [[ZleepManager sharedInstance] addZleepPoints:15];
-    self.totalZs.text = [@([[ZleepManager sharedInstance] totalZleepPoints]) stringValue];
+    //[[ZleepManager sharedInstance] addZleepPoints:5000];
+    //self.totalZs.text = [@([[ZleepManager sharedInstance] totalZleepPoints]) stringValue];
     
+}
+-(void) viewDidAppear:(BOOL)animated
+{
+    self.totalZs.text = [@([[ZleepManager sharedInstance] totalZleepPoints]) stringValue];
 }
 
 - (void)didReceiveMemoryWarning
